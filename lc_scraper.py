98a936291804fcb0d23a2dff0b8b355551ae95ed
Daniel Bak <DanielPBak@gmail.com>
2018-04-12 20:16:57 -0600

    Logging in

--- lc_scraper.py
+++ lc_scraper.py
@@ -1,4 +1,23 @@
-import lxml
+from lxml import html
 import requests
-url = "https://leetcode.com/contest/globalranking/1/"
-r = requests.get(url)
+login_url = "https://leetcode.com/accounts/login/"
+url = "https://leetcode.com/contest/globalranking/"
+page_num = 1
+
+
+with requests.Session() as s:
+    s.get(login_url)
+    if 'csrftoken' in s.cookies:
+        print('me!')
+        csrftoken = s.cookies['csrftoken']
+    else:
+        exit("no csrf token found ):")
+    # todo: use a config file for this stuff
+    data = {
+    "login":"dotsondots@gmail.com",
+    "password":"lookatmypasswordwow",
+    "csrfmiddlewaretoken":csrftoken
+    }
+    # log in
+    p = s.post(login_url, data=data, headers=dict(Referer=login_url))
+    
